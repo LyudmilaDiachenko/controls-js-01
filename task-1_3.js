ece74cde87daf7704ca484ed20d78d1749bcbf51
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-10-15 14:44:24 +0300

    add task 1 - 4

---
+++ task-1_3.js
@@ -0,0 +1,14 @@
+// Завдання 3 (2 бали)
+// Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+function findLongestWord (string) {
+    return string.split(' ').sort((a, b,) => b.length - a.length)[0]
+}
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
